b5f44de7a38a5b118f134129887a4152fad0b0be
Kai.W <z1191010@gmail.com>
2022-11-14 22:12:18 +0800

    Initial Commit

---
+++ Sango/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sango
+//
+//  Created by Kai on 2022/11/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
